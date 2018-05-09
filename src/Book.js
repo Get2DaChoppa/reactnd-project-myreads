174c1f100673e366c15cc858d9bde7d7b851bd08
Get2DaChoppa <s.g.bibas@gmail.com>
2018-05-10 00:53:01 +0300

    book thumbnail check

--- src/Book.js
+++ src/Book.js
@@ -8,24 +8,28 @@ function Book(props) {
     const title = book.title;
     const authors  = book.authors;
 
-    return (
-        <div className="book">
-            <div className="book-top">
-                <div
-                    className="book-cover"
-                    style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: `url(${url})`
-                    }}
-                >
+    if (url) {
+        return (
+            <div className="book">
+                <div className="book-top">
+                    <div
+                        className="book-cover"
+                        style={{
+                            width: 128,
+                            height: 193,
+                            backgroundImage: `url(${url})`
+                        }}
+                    >
+                    </div>
+                    <BookCtrl book={book} handler={props.handler}/>
                 </div>
-                <BookCtrl book={book} handler={props.handler}/>
+                <div className="book-title">{title}</div>
+                <div className="book-authors">{authors}</div>
             </div>
-            <div className="book-title">{title}</div>
-            <div className="book-authors">{authors}</div>
-        </div>
-    )
+        )
+    }else {
+        return null;
+    }
 }
 
 Book.propTypes = {
